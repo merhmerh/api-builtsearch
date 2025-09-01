f6344a2fa0eb627f99de5c72f10b0011c606c16f
arvin <arvinhow95@gmail.com>
2025-09-02 01:42:34 +0800

    feat: add authorization to webhook

--- src/routes/webhooks/auth/index.js
+++ src/routes/webhooks/auth/index.js
@@ -2,13 +2,21 @@ import { Hono } from "hono";
 import { grantProductOnSignUpCode } from "../../common/activate_signup_code.js";
 import { activeBuiltFormsMembers } from "../../common/active_builtforms_members.js";
 
+const DB_SECRET = process.env.SUPABASE_SECRET_KEY;
 const hook = new Hono();
 
 hook.post("/user-updated", async (c) => {
+	const authorization_header = c.req.header("authorization");
+
+	if (authorization_header !== DB_SECRET) {
+		console.log("Unauthorized request to auth webhook");
+		return c.json({ error: "Unauthorized" }, 401);
+	}
+
 	const data = await c.req.json();
 
+	console.log("webhook received: user-updated");
 	const { record, old_record } = data;
-
 	if (old_record.email_confirmed_at === null && record.email_confirmed_at !== null) {
 		console.log("User confirmed email");
 		await grantProductOnSignUpCode(record.id);
@@ -19,3 +27,5 @@ hook.post("/user-updated", async (c) => {
 });
 
 export const auth_webhook = hook;
+
+// api.builtsearch.com
